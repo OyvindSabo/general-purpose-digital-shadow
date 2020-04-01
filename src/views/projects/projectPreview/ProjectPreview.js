0bbf74044040f64c60b35fdaf77a4666508a24ae
oyvindsabo <oyvind.sabo@gmail.com>
2020-04-02 00:06:51 +0200

    [Fix #82] Add save and cancel button to ProjectNameInput$

--- src/views/projects/projectPreview/ProjectPreview.js
+++ src/views/projects/projectPreview/ProjectPreview.js
@@ -1,19 +1,13 @@
 const styled = include('src/libraries/styled/styled.js');
-const withShadow = include('src/styleWrappers/withShadow.js');
 const { div$ } = include('src/libraries/observableHtml/ObservableHtml.js');
 const ProjectPreviewContainer$ = include(
   'src/views/projects/projectPreviewContainer/ProjectPreviewContainer.js'
 );
 
 const ProjectPreview$ = (...children) =>
-  withShadow(ProjectPreviewContainer$)(
+  ProjectPreviewContainer$(
     styled({
-      color: 'dimgray',
-      height: '64px',
       padding: '0 16px',
-      background: 'white',
-      lineHeight: '64px',
-      fontSize: '16px',
     })(div$)(...children)
   );
 module.exports = ProjectPreview$;
