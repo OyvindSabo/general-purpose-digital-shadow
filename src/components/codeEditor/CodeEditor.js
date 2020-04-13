e334d8500404f9dcbde548990728543c1a3a872b
oyvindsabo <oyvind.sabo@gmail.com>
2020-04-13 12:08:50 +0200

    Fix stupid bug which deleted all content in code editor on input

--- src/components/codeEditor/CodeEditor.js
+++ src/components/codeEditor/CodeEditor.js
@@ -2,22 +2,24 @@ const styled = include('src/libraries/styled/styled.js');
 const { textArea$ } = include('src/libraries/observableHtml/ObservableHtml.js');
 const FullWidthCard$ = include('src/components/fullWidthCard/FullWidthCard.js');
 
-const CodeEditor$ = (value) =>
-  FullWidthCard$(
-    textArea$(value).setStyle({
-      padding: '20px',
-      width: '100%',
-      height: '100%',
-      fontFamily: '"Courier New", Courier, monospace',
-      fontSize: '14px',
-      lineHeight: '20px',
-      whiteSpace: 'nowrap',
-      border: 'none',
-      color: 'darkslategray',
-    })
-  ).setStyle({
+const CodeEditor$ = (value) => {
+  const textAreaElement = textArea$(value).setStyle({
+    padding: '20px',
+    width: '100%',
+    height: '100%',
+    fontFamily: '"Courier New", Courier, monospace',
+    fontSize: '14px',
+    lineHeight: '20px',
+    whiteSpace: 'nowrap',
+    border: 'none',
+    color: 'darkslategray',
+  });
+  const element = FullWidthCard$(textAreaElement).setStyle({
     height: 'calc(100% - 64px)',
     boxSizing: 'border-box',
   });
+  element.onInput = textAreaElement.onInput;
+  return element;
+};
 
 module.exports = CodeEditor$;
